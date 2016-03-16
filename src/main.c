6bb832829d93c74e000554cca4f17b5a524902b3
Peau Clement <clement.peau@epitech.eu>
2016-03-16 14:05:22 +0000

    Init

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in /home/peau_c/rendu/dante
+**
+** Made by Clement Peau
+** Login   <peau_c@epitech.net>
+**
+** Started on  Wed Mar 16 14:04:38 2016 Clement Peau
+** Last update Wed Mar 16 14:04:45 2016 Clement Peau
+*/
+
+int	main()
+{
+  return (0);
+}
